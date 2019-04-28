1a5ddeb0a6ab996549e300e816d0373f6e4b75d9
adho <adhoadhigal>
2019-04-28 17:24:19 +0300

    correct password

--- config.py
+++ config.py
@@ -3,19 +3,13 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:ouma12@localhost/pitcher2'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-
+    SENDER_MAIL ='adhoadhigal@gmail.com'
 
     @staticmethod
     def init_app(app):
@@ -28,12 +22,12 @@ class ProdConfig(Config):
 
 
 class TestConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://thomas:psqltoto29@localhost/pitchperfect_test'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adho:1234@localhost/pitch_test'
     pass
 
 
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:ouma12@localhost/pitcher2'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adho:1234localhost/blog'
     DEBUG = True
 
 config_options = {
