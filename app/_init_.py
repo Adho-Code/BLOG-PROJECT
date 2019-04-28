d2ef60b1e3d63c01e530f927d894c059ea87f7e9
adho <adhoadhigal>
2019-04-28 17:39:05 +0300

    create login form

--- app/_init_.py
+++ app/_init_.py
@@ -18,30 +18,26 @@ login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
 
 def create_app(config_name):
-
     app = Flask(__name__)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # Creating the app configurations
-
+    
     app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app)
-
-    # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
     simple.init_app(app)
+    login_manager.session_protection = 'strong'
+	login_manager.login_view = 'auth.login'
+	configure_uploads(app,photos)
+	
 
-    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
-    # configure UploadSet
+
     configure_uploads(app,photos)
 
     return app
