8ddba3f2c6045b97966530350f40cf58cf318a6a
adho <adhoadhigal>
2019-04-29 17:04:24 +0300

    deployement

--- config.py
+++ config.py
@@ -14,8 +14,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProdConfig(Config):
-    pass
-    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
         
 class TestConfig(Config):
     # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://adho:1234@localhost/pitch_test'
